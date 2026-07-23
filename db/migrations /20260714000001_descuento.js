eaaac1f0b3b1cf701ad5cdccc0bf367493625403
smithlaje <smithtasc.99@gmail.com>
2026-07-23 19:14:02 -0400

    Modify productos table to add protection fields

--- db/migrations /20260714000001_descuento.js
+++ db/migrations /20260714000001_descuento.js
@@ -1,13 +1,17 @@
 exports.up = function (knex) {
   return knex.schema.alterTable('productos', (t) => {
-    t.decimal('pct_descuento', 6, 2).notNullable().defaultTo(0);
-    t.decimal('precio_oferta_usd', 12, 2).notNullable().defaultTo(0);
+    t.string('marca').notNullable().defaultTo('');
+    t.decimal('pct_proteccion', 6, 2).notNullable().defaultTo(0);
+    t.boolean('proteccion_activa').notNullable().defaultTo(false);
+    t.decimal('precio_proteccion_usd', 12, 2).notNullable().defaultTo(0);
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.alterTable('productos', (t) => {
-    t.dropColumn('pct_descuento');
-    t.dropColumn('precio_oferta_usd');
+    t.dropColumn('marca');
+    t.dropColumn('pct_proteccion');
+    t.dropColumn('proteccion_activa');
+    t.dropColumn('precio_proteccion_usd');
   });
 };
